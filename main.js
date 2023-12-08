ec5c1a03c7ffd6b2d9cfb53615868664d0eb4826
kaungpyae1200 <kaungpyae120045@gmail.com>
2023-12-08 18:13:58 +0630

    update

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+import Typed from 'typed.js';
 import './style.css'
+
+const typeUi = document.querySelector("#typeUi");
+const headerUi  = document.querySelector("#headerUi");
+const header = new Typed(headerUi,{
+    strings : ["<span class='text-primary-700'>invest in the world’s potential</span> ","<span class='text-pink-700'>capital can unlock long-term value and drive economic growth</span> " ],
+    typeSpeed : 150,
+    backSpeed : 50,
+    backDelay : 2000,
+    startDelay :1500,
+    loop : true,
+
+});
+
+
+
+const typed = new Typed(typeUi,{
+    strings : ["<span class='text-primary-700'>Organization</span> ", "House", "  <span class='text-primary-700'>Companies</span> ",],
+    typeSpeed : 150,
+    backSpeed : 50,
+    backDelay : 2000,
+    startDelay :1500,
+    loop : true,
+
+
+
+})
