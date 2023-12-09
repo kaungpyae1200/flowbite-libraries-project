aab1dcf593cc155468c560e4edd1e4a831f4a9b5
kaungpyae1200 <kaungpyae120045@gmail.com>
2023-12-09 12:52:12 +0630

    update

--- main.js
+++ main.js
@@ -2,9 +2,25 @@ import Typed from 'typed.js';
 import './style.css';
 import AOS from 'aos';
 import WOW from "wow.js";
+import Glide from '@glidejs/glide'
+
+
 
 AOS.init();
-
+new Glide('.glide',{
+    type: 'carousel',
+    startAt: 0,
+    perView: 3,
+    focusAt :'center',
+    breakpoints : {
+        400: {
+            perView: 1,
+          },
+        800: {
+            perView: 2
+          }  
+    }
+}).mount()
 const typeUi = document.querySelector("#typeUi");
 const headerUi  = document.querySelector("#headerUi");
 // const menu = document.querySelector("#menu");
@@ -16,7 +32,16 @@ const headerUi  = document.querySelector("#headerUi");
 
 const wow = new WOW({
     boxClass:     'wow',      // animated element css class (default is wow)
-    animateClass: 'animate__animated', 
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
 });
 
 wow.init()
